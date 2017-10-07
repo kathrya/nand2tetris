7ef274acbe53a70f8aaa6dd4dc671fa889ff5b7f
kathrya <kathrya123@gmail.com>
2017-10-07 17:56:29 +0100

    Chapter 4 completed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,68 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Higher level thoughts:
+// A continuous loop running through all pixels, that checks at each step if keyboard is
+// pressed, then swap from blackening loop to whitening loop
+
+// White Loop: Check to see if should be black loop
+// Whiten bit, and advance by one (resetting if needed)
+
+// Initialise the end of the screen
+@24576
+D=A
+@end
+M=D
+
+// Reset pixel
+(RESET)
+@16384
+D=A
+@pixel
+M=D
+
+// White Loop
+(WHITE)
+@24576
+D=M
+@BLACK
+D;JNE
+// Whiten Pixel
+@pixel
+A=M
+M=0
+// increment
+@pixel
+M=M+1
+//Check if pixel > 24576, and reset
+D=M
+@end
+D=M-D
+@RESET
+D;JLT
+@WHITE
+0;JMP
+
+
+// Black Loop:
+(BLACK)
+@24576
+D=M
+@WHITE
+D;JEQ
+// Blacken Pixel
+@pixel
+A=M
+M=-1
+// increment
+@pixel
+M=M+1
+//Check if pixel > 24576, and reset
+D=M
+@end
+D=M-D
+@RESET
+D;JLT
+@BLACK
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// Higher level view:
+// some counter, i, and the answer, initialise at 0
+@i
+M=0			
+@R2
+M=0
+// while R3 < R1, add R0 to R2
+(LOOP)
+//									Do the Comparison
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JGE
+//									Now the addition part
+@R2
+D=M
+@R0
+D=D+M
+@R2
+M=D
+// 									Finally increment R3, and loop
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)                           	//End loop
